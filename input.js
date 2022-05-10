df731d6c300dd3f05189950ae98789da04983b18
Vera Nehvyadovich <veranehvadovic@gmail.com>
2022-05-10 16:33:14 +0300

    added second lab

---
+++ input.js
@@ -0,0 +1,165 @@
+//const clientHeight = window.innerHeight;
+//const clientWidth = window.innerWidth;
+const activeClass = 'target__active'
+
+let highLightedDiv, divToMove;
+
+let delayY = 0, delayX = 0;
+let mouseIsDown = false; // for move div
+let startCoordinates = {}; // for move div
+let actionType = '';
+
+
+/* function createCoordinates() {
+    return { x: Math.random() * (clientWidth - 1) + 1, y: Math.random() * (clientHeight - 1) + 1 };
+}
+
+function createSize() {
+    return { width: Math.random() * (500 - 100) + 100, height: Math.random() * (500 - 100) + 100 };
+} */
+
+
+function onClick(event) {
+    event.stopPropagation()
+
+    if(actionType != 'mousemove') {
+        if(highLightedDiv) highLightedDiv.classList.remove(activeClass);
+
+        highLightedDiv = event.target;
+        highLightedDiv.classList.add(activeClass)
+    }
+}
+
+function onWorkspaceClick(event) {
+    if(highLightedDiv && actionType != 'keydown'){
+        highLightedDiv.classList.remove(activeClass);
+        highLightedDiv = null;
+    }
+}
+
+function onMouseDown(event) {
+    actionType = event.type;
+
+    divToMove = event.target;
+    let size = divToMove.getBoundingClientRect();
+    delayY = event.clientY - size.top;
+    delayX = event.clientX - size.left;
+
+    startCoordinates.x = size.left;
+    startCoordinates.y = size.top;
+
+    mouseIsDown = true;
+}
+
+function onMouseUp(event) {
+    mouseIsDown = false;
+    divToMove = null;
+}
+
+function onMouseMove(event) {
+    actionType = event.type;
+
+    if(mouseIsDown) {
+        divToMove.style.top = `${event.clientY - delayY}px`;
+        divToMove.style.left = `${event.clientX - delayX}px`;
+    }
+}
+
+function onDoubleClick(event) {
+    onClick(event);
+    onMouseDown(event);
+}
+
+/*on keypress*/
+function onKeyPress(event) {
+    if(event.keyCode === 27 && divToMove) {
+        actionType = event.type
+        divToMove.style.top = `${startCoordinates.y}px`;
+        divToMove.style.left = `${startCoordinates.x}px`;
+        startCoordinates = {}
+        onMouseUp(event);
+    } else if(event.keyCode === 46 && highLightedDiv) {
+        highLightedDiv.remove()
+        highLightedDiv = null
+    }
+}
+
+function onTouchStart(event) {
+    divToMove = event.target;
+    let size = divToMove.getBoundingClientRect();
+    delayY = event.changedTouches[0].pageY - size.top;
+    delayX = event.changedTouches[0].pageX - size.left;
+
+    startCoordinates.x = size.left;
+    startCoordinates.y = size.top;
+
+    mouseIsDown = true;
+}
+
+function onTouchEnd(event) {
+    mouseIsDown = false;
+    divToMove = null;
+}
+
+function onTouchMove(event) {
+    if(mouseIsDown) {
+        [...event.changedTouches].forEach(touch => {
+            divToMove.style.top = `${touch.pageY - delayY}px`;
+            divToMove.style.left = `${touch.pageX - delayX}px`;
+        })
+    }
+}
+
+
+function addListeners(array) {
+    for(let element of array) {
+        element.addEventListener('click', onClick);
+        element.addEventListener('mousedown', onMouseDown);
+        element.addEventListener('dblclick', onDoubleClick);
+        element.addEventListener('touchstart', onTouchStart);
+    }
+}
+
+
+
+/*Function creates new 'divs'*/
+
+/* function addDivToWorkspace(event) {
+    newDiv = document.createElement('div');
+    newDiv.classList.add('target');
+
+    size = createSize();
+    coordinates = createCoordinates();
+
+    newDiv.style.width = `${size.width}px`;
+    newDiv.style.height = `${size.height}px`;
+
+    newDiv.style.left =  size.width + coordinates.x <= clientWidth ? `${coordinates.x}px`
+        : `${clientWidth - size.width}px`
+    newDiv.style.top = size.height + coordinates.y <= clientHeight ? `${coordinates.y}px`
+        : `${clientHeight - size.height}px`;
+
+    addListeners([newDiv]);
+    workspace.appendChild(newDiv);
+}
+ */
+
+document.addEventListener('touchmove', e => {
+    if (e.touches.length > 1) {  
+       e.preventDefault();
+    }
+}, {passive: false})
+
+
+//addDivBtn.addEventListener('click', addDivToWorkspace);
+const workspace = document.querySelector('.lock');
+
+workspace.addEventListener('click', onWorkspaceClick);
+workspace.addEventListener('mousemove', onMouseMove);
+workspace.addEventListener('mouseup', onMouseUp);
+workspace.addEventListener('touchmove', onTouchMove);
+workspace.addEventListener('touchend', onTouchEnd);
+
+window.addEventListener('keydown', onKeyPress);
+
+addListeners(document.querySelectorAll('.target'));
